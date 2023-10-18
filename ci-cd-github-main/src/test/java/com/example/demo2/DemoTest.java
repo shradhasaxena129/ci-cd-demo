b86c8a6443cf9493dae0a43b63693f2def5d53a4
knoldus <shradhasaxena129@gmail.com>
2023-10-18 11:42:35 +0530

    ci-cd

--- ci-cd-github-main/src/test/java/com/example/demo2/DemoTest.java
+++ ci-cd-github-main/src/test/java/com/example/demo2/DemoTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoTests {
+class DemoTest {
 
 	@Test
 	void contextLoads() {
